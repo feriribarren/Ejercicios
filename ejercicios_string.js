e8b1cf27989e36884dbff308853235e6ef25fd62
FerIribarren <64477772+feriribarren@users.noreply.github.com>
2020-05-05 20:54:38 -0300

    Add files via upload

---
+++ ejercicios_string.js
@@ -0,0 +1,61 @@
+// Ejercicios String
+// 1
+function contarEspacios(texto){
+ 	 let cantidad = 0
+
+ 	 for (i=0;i<texto.length;i++){
+ 	 	if (texto[i]===' '){
+ 	 		cantidad = cantidad + 1
+ 	 	}
+ 	 }
+ 	 return cantidad 
+ }
+
+ console.log(contarEspacios('Este texto tiene que tener 6 espacios'))
+
+
+// 2
+ function buscarTweets(mensajes, texto){
+    const listaTweets=[]
+    for (i=0;i<mensajes.length;i++){
+    	if (mensajes[i].includes(texto)){
+    		listaTweets.push(mensajes[i])
+    	}
+    }
+    return listaTweets
+ 	}
+
+ const tweets = [
+ 'Total de casos desde el comienzo de la pandemia en toda la provincia asciende a 243.',
+ 'El intendente aseguro que Rosario esta en condiciones de en uno o dos días flexibilizar algunos sectores.',
+ 'Rosario sumo una nueva jornada sin registrar nuevos casos de coronavirus.']
+
+console.log(buscarTweets(tweets,'Rosario'))
+
+
+// 3
+function censurarTexto(frase,listaPalabrasCensuradas){
+    let fraseCensurada=frase
+    
+    for (i=0;i<listaPalabrasCensuradas.length;i++){
+    	
+    	if (frase.includes(listaPalabrasCensuradas[i])){
+    		let caracteres=''
+    		for (j=0;j<listaPalabrasCensuradas[i].length;j++){
+    			caracteres=caracteres+'*'
+    		}
+
+    		fraseCensurada=fraseCensurada.replace(listaPalabrasCensuradas[i],caracteres)
+
+    	}
+
+    }
+    
+
+	return fraseCensurada
+}
+
+const citaLibro='Todos tenemos adentro una insospechada reserva de fortaleza que emerge cuando la vida nos pone a prueba'
+const censuras=['vida', 'fortaleza', 'prueba']
+
+console.log(censurarTexto(citaLibro,censuras))
